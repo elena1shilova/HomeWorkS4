8c9adac08b6a0bca2e43c1919f92fc57c5995558
elena1989 <elena.shilova89@gmail.com>
2022-06-28 17:10:53 +0300

    seminar 1

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+// Console.WriteLine("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine()); ;
+// if (numberA > numberB)
+// {
+//     Console.WriteLine("Большее число =>" + numberA);
+//     Console.WriteLine("Меньшее число =>" + numberB);
+// }
+// else
+// {
+//     Console.WriteLine("Большее число =>" + numberB);
+//     Console.WriteLine("Меньшее число =>" + numberA);
+// }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+// Console.WriteLine("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int numberC = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// if(numberA>numberB&&numberA>numberC) max = numberA;
+// else if(numberB>numberA&&numberB>numberC) max = numberB;
+// else max = numberC;
+// Console.WriteLine("Максимальное число из трех => " + max);
+
+
+// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+// Console.WriteLine("Введите число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// if(numberA%2==0) Console.WriteLine("Число "+numberA+ " четное");
+// else Console.WriteLine("Число "+numberA+" нечетное");
+
+
+// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+// Console.WriteLine("Введите число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Все четные числа до " + numberA + " => ");
+// for (int i = 2; i <= numberA; i += 2)
+// {
+//     Console.Write(i + " ");
+// }
