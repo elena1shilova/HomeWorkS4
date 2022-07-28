5b7212045ab74f6ac4c9dc70f95bb4ab047a2ef2
elena1989 <elena.shilova89@gmail.com>
2022-07-28 12:51:15 +0300

    seminar9

--- Program.cs
+++ Program.cs
@@ -1291,3 +1291,55 @@
 //         Console.WriteLine();
 //     }
 // }
+
+//SEMINAR9
+// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+// void numberS(int m, int n)
+// {
+//     for (int i = m; i <= n; i++)
+//     {
+//         if (m <= 0)
+//         {
+//             m = 1;
+//             i = 0;
+//         }
+//         else Console.Write($"{i} ");
+
+//     }
+// }
+// Console.Write("Введите минимальное число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// numberS(m, n);
+//------
+// Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+// void numberS(int m, int n)
+// {
+//     int summ = 0;
+//     int d = m;
+//     for (int i = m; i <= n; i++)
+//     {
+
+//         if (m <= 0)
+//         {
+//             m = 1;
+//             i = 0;
+//         }
+//         else summ += i;
+
+//     }
+//     Console.Write($"Сумма натуральных чисел от {d} до {n} => {summ}");
+// }
+// Console.Write("Введите минимальное число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// numberS(m, n);
+//----
